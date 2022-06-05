05aabaf4ef06d93db8505284a349a992ac6bc7bf
shunkominato <shun19871001@gmail.com>
2022-06-05 11:32:22 +0900

    add storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,29 +2,19 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: ['./tsconfig.json'],
     sourceType: 'module',
     tsconfigRootDir: __dirname,
-    ecmaFeatures: { jsx: true },
+    ecmaFeatures: {
+      jsx: true
+    }
   },
   plugins: ['@typescript-eslint', 'jsx-a11y', 'react', 'react-hooks'],
-  extends: [
-    'airbnb',
-    'airbnb-typescript',
-    'airbnb/hooks',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'next',
-    'next/core-web-vitals',
-    'prettier',
-  ],
+  extends: ['airbnb', 'airbnb-typescript', 'airbnb/hooks', 'eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/recommended-requiring-type-checking', 'plugin:react/recommended', 'plugin:react/jsx-runtime', 'next', 'next/core-web-vitals', 'prettier', 'plugin:storybook/recommended'],
   ignorePatterns: ['.eslintrc.*', 'next.config.*'],
   rules: {
     curly: ['error', 'all'],
@@ -32,6 +22,6 @@ module.exports = {
     'arrow-body-style': 'off',
     'no-unused-vars': 'off',
     'react/jsx-indent': 0,
-    'import/prefer-default-export': 'off',
-  },
+    'import/prefer-default-export': 'off'
+  }
 };
