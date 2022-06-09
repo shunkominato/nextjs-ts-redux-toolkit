1de7266f16665bfe4875dc02313109b2de151d30
shunkominato <shun19871001@gmail.com>
2022-06-09 23:02:40 +0900

    add button storybook

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,13 @@
+import React, { VFC } from 'react'
+
+interface Props {
+  label: string;
+  onClick: () => void;
+}
+export const Button: VFC<Props> = ({ label, onClick }) => {
+  return (
+    <button type="submit" onClick={onClick}>
+      {label}
+    </button>
+  );
+};
